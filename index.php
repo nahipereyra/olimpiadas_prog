38f3894e14328d0580628a60b56ba35d24d6f4ed
nahipereyra <nahipereyra2008@gmail.com>
2025-08-18 16:48:12 -0300

    esta es la pagina principal

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <header>
+        <div class="content">
+            <div class="menu container">
+                <a href="index.html" class="logo">logo</a>
+                <input type="checkbox" id="menu" />
+                <label for="menu">
+                    <img src="imgenes/imag.png" class="menu-icono" alt="Icono de menú">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="index.php">inicio</a></li>
+                        <li><a href="servicios.php">servicios</a></li>
+                        <li><a href="ofertas.">ofertas</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                categorias
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="categorias1.html" href="#">argentina</a></li>
+                                <li><a class="categorias2.html" href="#">latam</a></li>
+                                <li><a class="categorias3.html" href="#">jujuy</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="redes.html">REDES SOCIALES</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <h1> inicio </h1>
+</body>
+
+</html>

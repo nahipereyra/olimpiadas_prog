abfafdd7999508b7d5ba7208f7c745210fc2c1a8
nahipereyra <nahipereyra2008@gmail.com>
2025-08-18 16:58:54 -0300

    redes

---
+++ redes.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="content">
+            <div class="menu container">
+                <a href="index.html" class="logo">logo</a>
+                <input type="checkbox" id="menu"/>
+                <label for="menu">
+                    <img src="imgenes/imag.png" class="menu-icono" alt="Icono de menú">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="index.php">inicio</a></li>
+                        <li><a href="servicios.php">servicios</a></li>
+                        <li><a href="ofertas.php">ofertas</a></li>
+                        <li><a href="categorias.php">categorias</a></li>
+                        <li><a href="redes.php">REDES SOCIALES</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <h1> redes </h1>
+</body>
+</html>

976325004d61438f233f860f443d7ed5d07bb773
Bianca3-1 <Biancamariani512@gmail.com>
2025-11-07 15:11:32 -0300

    css/

--- ofertas.php
+++ ofertas.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>menu</title>
-    <link rel="stylesheet" href="ofertas.css">
+    <link rel="stylesheet" href="css/ofertas.css">
 </head>
 
 <div class="bodyy">

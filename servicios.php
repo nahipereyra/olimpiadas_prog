45e6bdc3fa2bc8fb8826b9704ec899f90c7b5bf6
Bianca3-1 <Biancamariani512@gmail.com>
2025-11-07 15:24:01 -0300

    new

--- servicios.php
+++ servicios.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>menu</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/servicios.css">
 </head>
 
 <body>

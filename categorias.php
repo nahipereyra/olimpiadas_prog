e588490723641e9831135918e103f051452d01dd
Bianca3-1 <Biancamariani512@gmail.com>
2025-11-07 15:27:07 -0300

    listo chavalas

--- categorias.php
+++ categorias.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>menu</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/categorias.css">
 </head>
 <body>
     <header>

37f038d6267d766084866d796f8995c333c43cdb
Bianca3-1 <Biancamariani512@gmail.com>
2025-11-07 15:21:44 -0300

    hola amiga

--- redes.php
+++ redes.php
@@ -5,40 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>menu</title>
-    <link rel="stylesheet" href="redes.css">
+    <link rel="stylesheet" href="css/redes.css">
 </head>
 
-<div class="body">
 
-    <body>
-        <header>
-            <div class="content">
-                <div class="menu container">
-                    <a href="index.html" class="logo">logo</a>
-                    <input type="checkbox" id="menu" />
-                    <label for="menu">
-                        <img src="imgenes/imag.png" class="menu-icono" alt="Icono de menú">
-                    </label>
-                    <nav class="navbar">
-                        <ul>
-                            <li><a href="index.php">inicio</a></li>
-                            <li><a href="servicios.php">servicios</a></li>
-                            <li><a href="ofertas.php">ofertas</a></li>
-                            <li><a href="categorias.php">categorias</a></li>
-                            <li><a href="redes.php">REDES SOCIALES</a></li>
-                        </ul>
-                    </nav>
-                </div>
+<body>
+    <header>
+        <div class="content">
+            <div class="menu container">
+                <a href="index.html" class="logo">logo</a>
+                <input type="checkbox" id="menu" />
+                <label for="menu">
+                    <img src="imgenes/imag.png" class="menu-icono" alt="Icono de menú">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="index.php">inicio</a></li>
+                        <li><a href="servicios.php">servicios</a></li>
+                        <li><a href="ofertas.php">ofertas</a></li>
+                        <li><a href="categorias.php">categorias</a></li>
+                        <li><a href="redes.php">REDES SOCIALES</a></li>
+                    </ul>
+                </nav>
             </div>
-        </header>
-        <div class="caja">
-            <img src="logo.png" alt="logo">
-            <h1>
-                Redes Sociales
-            </h1>
         </div>
-    </body>
+    </header>
+    <div class="caja">
+        <img src="logo.png" alt="logo">
+        <h1>
+            Redes Sociales
+        </h1>
+    </div>
+</body>
 
-</div>
+
 
 </html>

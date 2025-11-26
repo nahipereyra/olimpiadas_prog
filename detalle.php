8393ef52f69041fe336991f367c3d049103e58f4
camimaidana0 <camimaidana011@gmail.com>
2025-11-26 11:22:09 -0300

    Modificación de CAMILA

--- detalle.php
+++ detalle.php
@@ -30,9 +30,7 @@ $error_message = '';
     </style>
 </head>
 
-<body>
-
-    <body class="d-flex flex-column h-100">
+<body class="d-flex flex-column h-100">
         <header>
             <div class="content">
                 <div class="menu container">
@@ -257,6 +255,5 @@ $error_message = '';
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
             crossorigin="anonymous"></script>
-    </body>
 </body>
 </html>

e57645925e02df9afb4ea8938b4f9cdfbb166595
nahipereyra <nahipereyra2008@gmail.com>
2025-12-10 09:43:24 -0300

    se vienen cositas

--- registro.php
+++ registro.php
@@ -39,14 +39,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['ingresar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>login</title>
 </head>
 
 <body>
+    <body class="d-flex flex-column h-100">
+    <header>
+        <div class="content">
+            <div class="menu container">
+                <a href="index.html" class="logo">logo</a>
+                <input type="checkbox" id="menu" />
+                <label for="menu">
+                    <img src="imagenes/imag.png" class="menu-icono" alt="">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="index.php">inicio</a></li>
+                        <li><a href="servicios.php">servicios</a></li>
+                        <li><a href="ofertas.php">ofertas</a></li>
+                        <li><a href="categorias.php">categorias</a></li>
+                        <li><a href="redes.php">REDES SOCIALES</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
     <form method="POST" action="registro.php">
         <label for="correo">Nombre:</label>
         <input type="text" name="nombre" id="nombre">
-        
+
         <?php require_once 'componentes/componente-formulario.php' ?>
     </form>
 
